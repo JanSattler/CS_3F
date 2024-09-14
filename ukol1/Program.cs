20d27c1892d0efa39efaff926cdaee2e5c2906b1
pohrebacek <petrjan872@gmail.com>
2024-09-14 12:18:57 +0200

    ukol

---
+++ ukol1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+
+Point A = new Point();
+Point B = new Point(); 
+
+Console.Write("Zadej x pro A: ");
+A.X = Int32.Parse(Console.ReadLine());
+Console.Write("Zadej y pro A: ");
+A.Y = Int32.Parse(Console.ReadLine());
+
+Console.Write("Zadej x pro B: ");
+B.X = Int32.Parse(Console.ReadLine());
+Console.Write("Zadej y pro B: ");
+B.Y = Int32.Parse(Console.ReadLine());
+
+
+
+double distance = Math.Sqrt(Math.Pow(B.X - A.X, 2) + Math.Pow(B.Y - A.Y, 2));
+Console.WriteLine(distance);
