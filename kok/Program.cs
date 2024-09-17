aa66c1477e8fd9f46c6216f55d6f6574fe3c1b6b
Jan Sattler <jan.sattler@infis.cz>
2024-09-17 11:15:14 +0200

    dfgdgf

--- kok/Program.cs
+++ kok/Program.cs
@@ -1,6 +1,22 @@
-﻿Console.WriteLine("Zadej m: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Zadej a: ");
-int a = Convert.ToInt32(Console.ReadLine());
+﻿double[,] body = new double[4,2];
 
-Console.WriteLine(m*a);
+body[0, 0] = 1.0;
+body[0, 1] = 1.0;
+
+body[1, 0] = 4.0;
+body[1, 1] = 1.0;
+
+body[2, 0] = 4.0;
+body[2, 1] = 3.0;
+
+body[3, 0] = 1.0;
+body[3, 1] = 3.0;
+
+
+for (int i = 0; i < body.Length; i++)
+{
+    for (int j = 0; j < 2; j++)
+    {
+        Console.WriteLine(body[i, j]);
+    }
+}
