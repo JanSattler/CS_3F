2aeadbd8ef8892ad23f7aebf32a79875b700b887
jan.sattler <jan.sattler@infis.cz>
2024-09-19 12:48:41 +0200

    gfhfg

--- kok/Program.cs
+++ kok/Program.cs
@@ -1,4 +1,5 @@
-﻿double[,] body = new double[4,2];
+﻿double[,] body = new double[4, 2];
+
 
 body[0, 0] = 1.0;
 body[0, 1] = 1.0;
@@ -13,10 +14,8 @@ body[3, 0] = 1.0;
 body[3, 1] = 3.0;
 
 
-for (int i = 0; i < body.Length; i++)
+for (int i = 0; i < 4; i++)
 {
-    for (int j = 0; j < 2; j++)
-    {
-        Console.WriteLine(body[i, j]);
-    }
+    Console.WriteLine($"body[{body[i, 0]},{body[i, 1]}]");
 }
+    
