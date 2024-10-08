eac73e56f5b642f05907f0a83cecea93d9d964b9
Jan Sattler <jan.sattler@infis.cz>
2024-10-08 11:08:20 +0200

    gkfhg

---
+++ testBruh/Program.cs
@@ -0,0 +1,12 @@
+﻿for (int i = 0; i < 4; i++)
+{
+    Console.WriteLine("Napiš číslo: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    if (num < 0)
+    {
+        Console.WriteLine("Číslo je záporné");
+    } else
+    {
+        Console.WriteLine("Číslo je kladné");
+    }
+}
---
+++ testBruh2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Napiš číslo: ");
+int num = Convert.ToInt32(Console.ReadLine());
+check(num);
+
+static void check(int num)
+{
+    if (num%2 == 0)
+    {
+        Console.WriteLine("Číslo je sudé");
+    } else
+    {
+        Console.WriteLine("Číslo je liché");
+    }
+}
